82f708ca24f0fe616d09fba033f8940f2f340a12
Mohammed <sajidcurious9@gmail.com>
2025-02-19 17:54:37 -0800

    added ways to update the elements in an array

---
+++ list/update.py
@@ -0,0 +1,16 @@
+# Updating Elements into List
+# We can change the value of an element by accessing it using its index.
+
+a = [10, 20, 30, 40, 50]
+
+# Change the second element
+a[1] = 25 
+
+print(a) 
+
+b = [10, 20, 30, 40, 50]
+
+# Change the last element
+b[-1] = 80 
+
+print(b) 
