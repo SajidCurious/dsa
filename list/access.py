b5a6de568d3753a9827477449e4cce8c3cc73e3f
Mohammed <sajidcurious9@gmail.com>
2025-02-19 17:46:04 -0800

    added ways to access the elements of an array

---
+++ list/access.py
@@ -0,0 +1,16 @@
+# Accessing List Elements
+# Elements in a list can be accessed using indexing. Python indexes start at 0, so a[0] will access the first element, while negative indexing allows us to access elements from the end of the list. Like index -1 represents the last elements of list.
+
+a = [10, 20, 30, 40, 50]
+
+# Access first element
+print(a[0])
+
+# Access second element
+print(a[1]) 
+
+# Access last element
+print(a[-1])
+
+# Access second element from the last
+print(a[-2])
