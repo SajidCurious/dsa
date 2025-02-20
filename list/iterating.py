606f5ecb2d24b8817f4991f7385436311e219cf2
Mohammed <sajidcurious9@gmail.com>
2025-02-19 18:26:19 -0800

    iterated an array using the for loop

---
+++ list/iterating.py
@@ -0,0 +1,7 @@
+# Iterating Over Lists
+# We can iterate the Lists easily by using a for loop or other iteration methods. Iterating over lists is useful when we want to do some operation on each item or access specific items based on certain conditions. Let’s take an example to iterate over the list using for loop.
+
+a = ["Banana","Mango","Pineapple"]
+
+for item in a:
+    print(item)
