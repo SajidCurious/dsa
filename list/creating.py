f63d2a65b4922acf8b7c8bc2ab61fe8f80c3c652
Mohammed <sajidcurious9@gmail.com>
2025-02-19 17:42:29 -0800

    added another method

--- list/creating.py
+++ list/creating.py
@@ -13,3 +13,12 @@ c = [1, 'hello', 3.14, True]
 print(a)
 print(b)
 print(c)
+
+# Using list() Constructor
+# We can also create a list by passing an iterable (like a string, tuple, or another list) to list() function.
+
+# From a tuple
+r = list((1, 2, 3, 'apple', 4.5))  
+
+print(r)
+
