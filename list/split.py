87bb20ade4d781cd183a848abe9d484226e5f772
Mohammed <sajidcurious9@gmail.com>
2025-02-21 20:43:13 -0800

    example to take user input and by using split method converting it to the list

---
+++ list/split.py
@@ -0,0 +1,11 @@
+# Get list as input Using split() Method
+
+# Get user input and split it into a list
+# user_input = input("Enter elements separated by space: ").split()
+
+# print("List:", user_input)
+
+
+# second example
+input = input("Enter letters with space: ").split()
+print("List:", input)
